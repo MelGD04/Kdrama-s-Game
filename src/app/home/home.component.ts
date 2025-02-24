898f05c2122231bcdbde9b182644dfb81a414028
Mel04GD <mely301104@gmail.com>
2025-02-24 09:18:05 -0500

    somethings new

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../card/card.component';
-import {MatSidenavModule} from '@angular/material/sidenav'; 
 
 @Component({
   selector: 'app-home',
   imports:[
-    CardComponent,
-    MatSidenavModule
+    CardComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
--- src/app/night-as-come/night-as-come.component.ts
+++ src/app/night-as-come/night-as-come.component.ts
@@ -20,6 +20,33 @@ export class NightAsComeComponent{
       }
     }
     
+  currentContent: string = 'content1'; // Inicialmente muestra el primer encabezado (índice 0)
 
+  contentTags: string[] = ['content1', 'content2', 'content3', 'content4'];
+
+  nextContent(): void {
+    const currentIndex = this.contentTags.indexOf(this.currentContent);
+    const nextIndex = (currentIndex + 1) % this.contentTags.length; // Cicla hacia adelante
+    this.showContent(this.contentTags[nextIndex]);
+  }
+
+  previousContent(): void {
+    const currentIndex = this.contentTags.indexOf(this.currentContent);
+    const previousIndex = (currentIndex - 1 + this.contentTags.length) % this.contentTags.length; // Cicla hacia atrás
+    this.showContent(this.contentTags[previousIndex]);
+  }
+
+  showContent(title: string): void {
+    console.log('Cambiando a:', title);
+    this.currentContent = title;
+  }
   
+  users = [
+    { name: '1' }, { name: '2' }, {name: '3'}, {name: '4'}, {name: '5'},
+    { name: '6' }, { name: '7' }, {name: '8'}, {name: '9'}, {name: '10'},
+    { name: '11' }, { name: '12' }, {name: '13'}, {name: '14'}, {name: '15'},
+    { name: '16' }, { name: '17' }, { name: '18' }, { name: '19' }, { name: '20' },
+    { name: '21' }, { name: '22' }, { name: '23' }, { name: '24' }, { name: '25' },
+    { name: '26' }, { name: '27' }, { name: '28' }, { name: '29' }, { name: '30' }
+  ]
 }
